64b77774b6ce0a9b1a09ab95aa68fc39bf1c925f
Truong-67 <nguyentruongcorem@gmail.com>
2026-05-02 15:12:15 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -342,7 +342,7 @@ export default function App() {
     .then(data => {
       setThongKeData(data || []);
     })
-    console.log('THONG KE DATA:', data);
+    
     .catch(() => {
       setThongKeData([]);
     });
